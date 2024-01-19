5b81d0cff719aa4e3975745e22b56392d93115ea
rioakash23 <142964252+rioakash23@users.noreply.github.com>
2024-01-19 10:50:07 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,72 @@
+
+
+
+    function one()
+    {
+      var num1 = Number(document.getElementById("startval").value);
+      var num2 = Number(document.getElementById("endval").value);
+      var sum = num1 + num2;
+      document.getElementById("res").innerHTML=sum;
+      
+  
+    }
+
+    function two() {
+        var num1 = Number(document.getElementById("startval").value);
+      var num2 = Number(document.getElementById("endval").value);
+      var sum = num1 - num2;
+      document.getElementById("res").innerHTML=sum;
+      
+    }
+
+    function three(){
+        var num1 = Number(document.getElementById("startval").value);
+        var num2 = Number(document.getElementById("endval").value);
+        var sum = num1 * num2;
+        document.getElementById("res").innerHTML=sum;
+    }
+
+    function four(){
+        var num1 = Number(document.getElementById("startval").value);
+        var num2 = Number(document.getElementById("endval").value);
+        var sum = num1 ** num2;
+        document.getElementById("res").innerHTML=sum;
+    }
+
+
+    
+    function five(){
+        var num1 = Number(document.getElementById("startval").value);
+        var num2 = Number(document.getElementById("endval").value);
+        var sum = num1 / num2;
+        document.getElementById("res").innerHTML=sum;
+    }
+
+        
+    function six(){
+        var num1 = Number(document.getElementById("startval").value);
+        var num2 = Number(document.getElementById("endval").value);
+        var sum = num1 % num2;
+        document.getElementById("res").innerHTML=sum;
+    }
+
+    function seven(){
+        var num1 = Number(document.getElementById("startval").value);
+        var num2 = Number(document.getElementById("endval").value);
+        var sum = num1 + num2 
+        document.getElementById("res").innerHTML=sum;
+    }
+
+    function eight(){
+        var num1 = Number(document.getElementById("startval").value);
+        var num2 = Number(document.getElementById("endval").value);
+        var sum = num1 - num2 
+        document.getElementById("res").innerHTML=sum;
+    }
+
+    function nine(){
+        var num1 = Number(document.getElementById("startval").value);
+        var num2 = Number(document.getElementById("endval").value);
+        var sum = num1 ** num2 
+        document.getElementById("res").innerHTML=sum;
+    }
